6c3a5b7b326fee3aaa3d779d568d49ec639dda3d
yakhlafhoussam <yakhlafhoussam@.com>
2026-04-19 23:44:04 +0100

    feat(routes): add chat, order, like, comment, delete, and admin routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BanController;
+use App\Http\Controllers\ChatController;
 use App\Http\Controllers\DdosController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MarketplaceController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderPageController;
 use App\Http\Controllers\PasswordController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
@@ -34,6 +37,13 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth', 'check.banned', 'check.profile'])->group(function () {
 
+    Route::get('/chats', [ChatController::class, 'list'])->name('chat.list');
+
+    Route::get('/orders', [OrderPageController::class, 'index'])->name('orders');
+
+    Route::get('/chat/{user}', [ChatController::class, 'index'])->name('chat.with');
+    Route::post('/chat/{user}', [ChatController::class, 'send'])->name('chat.send');
+
     Route::get('/', [HomeController::class, 'index'])->name('home');
 
     Route::post('/profile/update-image', [ProfileController::class, 'updateImage'])->name('profile.update.image');
@@ -46,6 +56,7 @@ Route::middleware(['auth', 'check.banned', 'check.profile'])->group(function () 
     Route::get('/service/create', [ServicesController::class, 'create'])->name('create.service');
     Route::post('/service/create', [ServicesController::class, 'store'])->name('services.store');
     Route::get('/service/show/{service}', [ServicesController::class, 'show'])->name('show.service');
+    Route::delete('/service/delete/{service}', [ServicesController::class, 'destroy'])->name('service.delete');
 
     Route::get('/service/ddos', [ServicesController::class, 'ddos'])->name('ddos');
     Route::get('/service/ddos/check', [DdosController::class, 'apply'])->name('ddos.check');
@@ -61,8 +72,23 @@ Route::middleware(['auth', 'check.banned', 'check.profile'])->group(function () 
     Route::get('/marketplace/create', [MarketplaceController::class, 'create'])->name('create.marketplace');
     Route::post('/marketplace/create', [MarketplaceController::class, 'store'])->name('marketplace.store');
     Route::get('/marketplace/show/{product}', [MarketplaceController::class, 'show'])->name('show.product');
+    Route::delete('/marketplace/delete/{product}', [MarketplaceController::class, 'destroy'])->name('marketplace.delete');
+    Route::post('/marketplace/order/{product}', [OrderController::class, 'storeMarketplace'])->name('marketplace.order');
+
+    Route::post('/service/order/{service}', [OrderController::class, 'storeService'])->name('service.order');
 
     Route::get('/posts', [PostController::class, 'index'])->name('posts');
     Route::get('/posts/create', [PostController::class, 'create'])->name('create.post');
     Route::post('/posts/create', [PostController::class, 'store'])->name('posts.store');
+
+    Route::post('/posts/{post}/like', [PostController::class, 'like'])->name('posts.like');
+    Route::delete('/posts/{post}/unlike', [PostController::class, 'unlike'])->name('posts.unlike');
+
+    Route::post('/posts/{post}/comments', [PostController::class, 'storeComment'])->name('comments.store');
+
+    Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.delete');
+    Route::delete('/comments/{comment}', [PostController::class, 'destroyComment'])->name('comments.delete');
 });
+
+// Admin routes
+require __DIR__.'/admin.php';
